86bc3f5a6cf6dca230feefb1510c3484e7d9f6d7
Cory McCartan <cory.mccartan@gmail.com>
2013-07-02 10:16:46 -0700

    constants for PWM, Relay, Analog, Solenoid, Digital IO, etc.

--- src/com/shsrobotics/library/GLOBAL.java
+++ src/com/shsrobotics/library/GLOBAL.java
@@ -1,5 +1,6 @@
 package com.shsrobotics.library;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Relay;
 
 /**
@@ -7,22 +8,80 @@ import edu.wpi.first.wpilibj.Relay;
  * @author Team 2412 <first.robototes.com, github.com/robototes>
  */
 public interface GLOBAL {
-	/** USB port 1.		*/
-	public static final int USB_1 = 1;
-	
-	/** USB port 2.		*/
-	public static final int USB_2 = 2;
-	
-	/** USB port 3.		*/
-	public static final int USB_3 = 3;
-	
-	/** USB port 4.		*/
-	public static final int USB_4 = 4;
-	
 	/** For robots with no gyroscope. */
 	public static final double noGyroscopeAngle = 0.0;
 	
-	//universal constants
+	/**	 USB ports.	 */
+	public static final int 
+		USB_1 = 1,
+		USB_2 = 2,
+		USB_3 = 3,
+		USB_4 = 4;
+	
+	/**	 PWM Ports.	 */
+	public static final int
+		PWM_1 = 1,
+		PWM_2 = 2,
+		PWM_3 = 3,
+		PWM_4 = 4,
+		PWM_5 = 5,
+		PWM_6 = 6,
+		PWM_7 = 7,
+		PWM_8 = 8,
+		PWM_9 = 9,
+		PWM_10 = 10;
+	
+	/**	 Analog inputs.	 */
+	public static final int
+		ANALOG_1 = 1,
+		ANALOG_2 = 2,
+		ANALOG_3 = 3,
+		ANALOG_4 = 4,
+		ANALOG_5 = 5,
+		ANALOG_6 = 6,
+		ANALOG_7 = 7,
+		ANALOG_8 = 8;
+	
+	/**	 Solenoid outputs.	 */
+	public static final int
+		SOLENOID_1 = 1,
+		SOLENOID_2 = 2,
+		SOLENOID_3 = 3,
+		SOLENOID_4 = 4,
+		SOLENOID_5 = 5,
+		SOLENOID_6 = 6,
+		SOLENOID_7 = 7,
+		SOLENOID_8 = 8;
+	
+	/**	 Relay outputs.	 */
+	public static final int
+		RELAY_1 = 1,
+		RELAY_2 = 2,
+		RELAY_3 = 3,
+		RELAY_4 = 4,
+		RELAY_5 = 5,
+		RELAY_6 = 6,
+		RELAY_7 = 7,
+		RELAY_8 = 8;
+	
+	/**	 Digital inputs/outputs.	 */
+	public static final int
+		DIGITAL_IO_1 = 1,
+		DIGITAL_IO_2 = 2,
+		DIGITAL_IO_3 = 3,
+		DIGITAL_IO_4 = 4,
+		DIGITAL_IO_5 = 5,
+		DIGITAL_IO_6 = 6,
+		DIGITAL_IO_7 = 7,
+		DIGITAL_IO_8 = 8,
+		DIGITAL_IO_9 = 9,
+		DIGITAL_IO_10 = 10,
+		DIGITAL_IO_11 = 11,
+		DIGITAL_IO_12 = 12,
+		DIGITAL_IO_13 = 13,
+		DIGITAL_IO_14 = 14;
+	
+	/**	 Constants for relays.	 */
 	public static final Relay.Value 
 		ON = Relay.Value.kForward, 
 		OFF = Relay.Value.kOff, 
@@ -30,4 +89,9 @@ public interface GLOBAL {
 		CLOSED = Relay.Value.kOff,
 		FORWARD = Relay.Value.kForward,
 		REVERSE = Relay.Value.kReverse;
+	
+	/**	 Constants for pneumatics.	 */
+	public static final DoubleSolenoid.Value
+		EXTENDED = DoubleSolenoid.Value.kForward,
+		RETRACTED = DoubleSolenoid.Value.kReverse;
 }
