b8a873df9b9f1c1e007927057d0175d731977915
MaxOrth <max.orth96@gmail.com>
2015-01-20 21:38:45 -0800

    Updated to work more smoothly with variable parameter stuff.

--- src/com/shsrobotics/library/PermissionAction.java
+++ src/com/shsrobotics/library/PermissionAction.java
@@ -1,19 +1,25 @@
 package com.shsrobotics.library;
 
-import java.util.function.BooleanSupplier;
+import java.util.function.Predicate;
 
 
 public class PermissionAction {
 	public VoidFunction action;
-	public java.util.function.BooleanSupplier permFunc;
+	public Predicate<Object[]> permFunc;
 	
-	public PermissionAction(VoidFunction func, BooleanSupplier perm) {
+	public PermissionAction(VoidFunction func, Predicate<Object[]> perm) {
 		action = func;
 		permFunc = perm;
 	}
 	
-	public void perform() {
-		if ( permFunc.getAsBoolean() ) action.f();
+	public void perform(Object...args) {
+		try {
+			if ( permFunc.test(args) ) action.f(args);
+		}
+		catch ( ClassCastException cce ) {
+			System.err.println("Check parameter types more correctly!");
+			cce.printStackTrace();
+		}
 	}
 	
 }
--- src/com/shsrobotics/library/VoidFunction.java
+++ src/com/shsrobotics/library/VoidFunction.java
@@ -2,5 +2,5 @@ package com.shsrobotics.library;
 
 @FunctionalInterface
 public interface VoidFunction {
-	public void f();
+	public void f(Object...args );
 }
